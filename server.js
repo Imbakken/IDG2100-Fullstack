f3bd8651b4618496debafaa0a2c712f8dace259d
Imbakken <imbakken@stud.ntnu.no>
2023-03-09 14:00:18 +0100

    ...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-//const cors = require("cors");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DB_CONNECT, () => console.log("Database connected")
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 app.use("/api/brew", brewRoutes);
 app.use("/api/bean", beanRoutes);
 app.use("/api/rate", rateRoutes);
