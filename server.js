6e7895ee68f130492f90dbdb1593e238a3bc2301
Imbakken <imbakken@stud.ntnu.no>
2023-03-12 19:31:28 +0100

    .

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -33,12 +34,13 @@ app.use("/api/rate", rateRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`server is up and running on ${PORT}`));
-
-
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
     app.use(express.static('client/build'));
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
+
+app.listen(PORT, () => console.log(`server is up and running on ${PORT}`));
+
+
