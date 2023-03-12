8c087974bac2e1a1693b6cf46d19995bf612906c
Imbakken <imbakken@stud.ntnu.no>
2023-03-12 20:00:02 +0100

    .

--- server.js
+++ server.js
@@ -19,11 +19,7 @@ mongoose.connect(process.env.DB_CONNECT, () => console.log("Database connected")
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());  
-app.use(cors({
-    origin: "https://idg2100-fullstack.onrender.com/"
-}
-))
-app.options('*', cors())
+app.use(cors());
 app.use("/api/brew", brewRoutes);
 app.use("/api/bean", beanRoutes);
 app.use("/api/rate", rateRoutes);
