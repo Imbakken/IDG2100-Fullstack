31fc53041d47f707f42b5c812b9f7f4b7dd3138e
Imbakken <imbakken@stud.ntnu.no>
2023-03-15 19:50:06 +0100

    .

--- server.js
+++ server.js
@@ -31,11 +31,12 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+/*
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
-
+*/
 app.listen(PORT, () => console.log(`server is up and running on ${PORT}`));
 
 
