fa51264b7465d486503019a0153b330ec09ec985
Ida Marie Joakimsen Bakken <72867291+Imbakken@users.noreply.github.com>
2023-04-11 12:06:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ app.get('/', function(req, res) {
 
     res.send('cors problem fixed:)');
 });
-
+/*
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
+  }*/
 
 app.listen(PORT, () => console.log(`server is up and running on ${PORT}`));
 
